86db9015683c4d561ffeaf645bafd74ea9d819ac
Vladimir Ivanov <ivanov_va@hotmail.com>
2019-02-02 12:26:43 +0200

    ios icons update

--- Concord/Concord.Android/MainActivity.cs
+++ Concord/Concord.Android/MainActivity.cs
@@ -6,16 +6,16 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Droid;
 
-namespace Concord.Droid
-{
+namespace Concord.Droid {
     [Activity(Label = "Concord", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
+        protected override void OnCreate(Bundle savedInstanceState) {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
