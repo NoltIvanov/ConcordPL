a925243bfc1b8a9cd02f6575583bf6d06aa32b97
Vladimir Ivanov <ivanov_va@hotmail.com>
2019-02-02 11:29:26 +0200

    update

---
+++ Concord/Concord/ApplicationConst.cs
@@ -0,0 +1,5 @@
+﻿namespace Concord {
+    internal static class ApplicationConst {
+        internal const string MAIN_HOST = "new.concord-shop.pl";
+    }
+}
---
+++ Concord/Concord/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Concord {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+
+            CheckInternetConnection(CrossConnectivity.IsSupported);
+        }
+
+        protected override void OnAppearing() {
+            base.OnAppearing();
+
+            refreshButton.Clicked += OnRefreshButton;
+        }
+
+        protected override void OnDisappearing() {
+            base.OnDisappearing();
+
+            refreshButton.Clicked -= OnRefreshButton;
+        }
+
+        private async void OnRefreshButton(object sender, EventArgs e) {
+            try {
+                if (CrossConnectivity.Current.IsConnected) {
+                    webView.IsVisible = await CrossConnectivity.Current.IsReachable(ApplicationConst.MAIN_HOST);
+                    webView.Source = "http://new.concord-shop.pl/pl-pl";
+                }
+            }
+            catch (Exception) {
+                Debugger.Break();
+            }
+        }
+
+        private async void CheckInternetConnection(bool isSupported) {
+            try {
+                if (isSupported) {
+                    if (CrossConnectivity.Current.IsConnected) {
+                        webView.IsVisible = await CrossConnectivity.Current.IsReachable(ApplicationConst.MAIN_HOST);
+                    } else {
+                        webView.IsVisible = false;
+                    }
+                }
+            }
+            catch (Exception) {
+                Debugger.Break();
+            }
+        }
+    }
+}
